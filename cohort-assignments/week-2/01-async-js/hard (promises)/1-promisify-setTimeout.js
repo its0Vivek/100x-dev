20cc2774aee5a22cdbeacd98d0f0c285ca86b6dd
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-18 22:50:00 +0530

    complete hard

--- cohort-assignments/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ cohort-assignments/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,14 @@
 */
 
 function wait(n) {
+    const milliseconds = n * 1000; // Convert seconds to milliseconds
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("hello World");
+            resolve();
+        }, milliseconds);
+    });
 }
 
 module.exports = wait;
+
