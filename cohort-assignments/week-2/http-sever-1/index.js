404d24b4e92d4c1546aec6d4fa924ad88a693ef2
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-21 09:33:30 +0530

    created a http server

--- cohort-assignments/week-2/http-sever-1/index.js
+++ cohort-assignments/week-2/http-sever-1/index.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 const port = 3000
+app.use(bodyparser.json())
 
+console.log(app)
 
 app.get("/route-handler", function (req, res) {
     res.send("hello world")
 })
+app.post('/', function (req, res) {
+    const message = req.body.name;
+    console.log(message);
+})
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+app.post('/home', function (req, res) {
+    console.log(req.data)
+    res.send('<b> hi there <b>')
 })
 
 app.listen(port, function () {
