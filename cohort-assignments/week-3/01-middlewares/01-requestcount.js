f1a7a8fc1f8c57446058d42564a5370a9715ec5f
Vivek Mali <148007729+its0Vivek@users.noreply.github.com>
2024-03-28 23:59:42 +0530

    middlewar 1 assignment is  done

--- cohort-assignments/week-3/01-middlewares/01-requestcount.js
+++ cohort-assignments/week-3/01-middlewares/01-requestcount.js
@@ -10,16 +10,29 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
+function userMiddleware(req, res, next) {
+  requestCount++;
+  next()
+}
+
+
+
+app.use(userMiddleware)
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
+  console.log(requestCount)
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
+  console.log(requestCount)
 });
 
-app.get('/requestCount', function(req, res) {
+app.get('/requestCount', function (req, res) {
   res.status(200).json({ requestCount });
+  console.log(requestCount)
 });
 
 module.exports = app;
+app.listen(3000)
